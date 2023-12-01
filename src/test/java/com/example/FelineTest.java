35d9d37411f1b65c73e0688fc764a0bd7abfa6a0
Elena Lyamina <elen.iva89@gmail.com>
2023-12-02 00:41:40 +0300

    Homework for project Sprint 6

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,2 +1,31 @@
-package com.example;public class FelineTest {
+package com.example;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+
+    Feline feline = new Feline();
+
+    @Test
+    public void getFamilyTest(){
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+    @Test
+    public void getKittensNoParamTest(){
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensParamTest(){
+        assertEquals(3, feline.getKittens(3));
+    }
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        assertEquals(Arrays.asList("Животные", "Птицы", "Рыба"), feline.eatMeat());
+    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,2 +1,47 @@
-package com.example;public class LionTest {
+package com.example;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1,  lion.getKittens());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        List<String> expected = Arrays.asList("Животные", "Птицы", "Рыба");
+
+        Mockito.when(feline.getFood("Хищник")).thenReturn(expected);
+        assertEquals(expected, lion.getFood());
+    }
+
+    @Test
+    public void LionThrowsExceptionTest() {
+        assertThrows("Используйте допустимые значения пола животного - самей или самка", Exception.class, () -> new Lion("InvalidSex", feline));
+
+    }
 }
